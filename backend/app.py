938200451d309092b8be474cb558e3df13fcf74b
DanyHidalgo <danielhl@ufm.edu>
2023-10-09 20:10:50 -0600

    Actualizacion Barra de busqueda

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from models import db, Recipe, RecipeIngredient, RecipeComment, RecipeStep
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from sqlalchemy import or_
 import json
 
